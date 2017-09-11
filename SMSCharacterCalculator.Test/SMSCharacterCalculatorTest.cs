b2f7f1fa9e4491093e637564eeeefca9a3e0dd7d
János Orcsik <orcsikj@gmail.com>
2017-09-11 11:10:36 +0200

    First commit

---
+++ SMSCharacterCalculator.Test/SMSCharacterCalculatorTest.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SMSCharacterCalculator.Test
+{
+    [TestClass]
+    public class SMSCharacterCalculatorTest
+    {
+        [TestMethod]
+        public void CalculateLength_With_Null()
+        {
+            string text = null;
+
+            int length = SMSCharacterCalculator.CalculateLength(text);
+
+            Assert.IsTrue(length == 0);
+        }
+
+        [TestMethod]
+        public void CalculateLength_With_Empty()
+        {
+            string text = string.Empty;
+
+            int length = SMSCharacterCalculator.CalculateLength(text);
+
+            Assert.IsTrue(length == 0);
+        }
+
+        [TestMethod]
+        public void CalculateLength_With_One_Character()
+        {
+            string text = "a";
+
+            int length = SMSCharacterCalculator.CalculateLength(text);
+
+            Assert.IsTrue(length == 1);
+        }
+
+        [TestMethod]
+        public void CalculateLength_With_Special_Character()
+        {
+            string[] specialCharacters =
+            {
+                "|", "^", "€", "{", "}", "[", "~", "]", "\\"
+            };
+
+            foreach (string s in specialCharacters)
+            {
+                int length = SMSCharacterCalculator.CalculateLength(s);
+
+                Assert.IsTrue(length == 2);
+            }
+        }
+
+        [TestMethod]
+        public void OptimizeText_With_Null()
+        {
+            string text = null;
+
+            string actual = SMSCharacterCalculator.OptimizeText(text);
+
+            Assert.IsTrue(text == actual);
+
+        }
+
+        [TestMethod]
+        public void OptimizeText_With_Empty()
+        {
+            string text = string.Empty;
+
+            string actual = SMSCharacterCalculator.OptimizeText(text);
+
+            Assert.IsTrue(text == actual);
+        }
+
+        [TestMethod]
+        public void OptimizeText_With_One_Character()
+        {
+            string text = "a";
+
+            string actual = SMSCharacterCalculator.OptimizeText(text);
+
+            Assert.IsTrue(text == actual);
+        }
+
+        [TestMethod]
+        public void OptimizeText_With_Unicode_Characters()
+        {
+            string[] unicodeCharacters =
+            {
+                "á",
+                "í",
+                "ó",
+                "ú",
+                "ő",
+                "ű",
+                "Á",
+                "Í",
+                "Ú",
+                "Ó",
+                "Ő",
+                "Ű"
+            };
+
+            string[] gsmCharacters =
+            {
+                "à",
+                "ì",
+                "ò",
+                "ù",
+                "ö",
+                "ü",
+                "Å",
+                "I",
+                "U",
+                "O",
+                "Ö",
+                "Ü"
+            };
+
+            for (int i = 0; i < unicodeCharacters.Length; i++)
+            {
+                string actual = SMSCharacterCalculator.OptimizeText(unicodeCharacters[i]);
+
+                Assert.IsTrue(actual == gsmCharacters[i]);
+            }
+        }
+
+        [TestMethod]
+        public void GetSMSValidation_With_One_SMS_With_Unicode_AND_Special_Character()
+        {
+            string text = "{Í}";
+            string expected = "{I}";
+
+            SMSCharacterCalculator.SMSValidation actual = SMSCharacterCalculator.GetSMSValidation(text);
+
+            Assert.IsFalse(actual.IsLonger);
+            Assert.IsTrue(actual.TextLength == 5);
+            Assert.IsTrue(actual.SMSCount == 1);
+            Assert.IsTrue(actual.OptimizedText == expected);
+        }
+
+        [TestMethod]
+        public void GetSMSValidation_With_Two_SMS()
+        {
+            string text =
+                "{abvgfhtrururufhdfjhdjfhjdjfhjdfhjkfhjksuizueuruiewrziwuezrizweiurzwuiezruiweuirweuizruiwezruizweuirwzeurzweuiriuwzeuirrwezruizweuiriuwzruizwiuezriuzweuirziuzzzz}";
+
+            SMSCharacterCalculator.SMSValidation actual = SMSCharacterCalculator.GetSMSValidation(text);
+
+            Assert.IsFalse(actual.IsLonger);
+            Assert.IsTrue(actual.TextLength == 164);
+            Assert.IsTrue(actual.SMSCount == 2);
+            Assert.IsTrue(actual.OptimizedText == text);
+        }
+
+        [TestMethod]
+        public void GetSMSValidation_With_Three_SMS()
+        {
+            string text =
+                "{abvgfhtrururufhdfjhdjhfghfggfhfhiiiourzoruozuiorutiozuouuzuiortuziouriozuriozuiourzirtuziuituziouriotuzurzuriotuziutruziorioutruziutiuriturtzotioziortuzioiozutzutruzfhjdjfhjdfhjkfhjksuizueuruiewrziwuezrizweiurzwuiezruiweuirweuizruiwezruizweuirwzeurzweuiriuwzeuirrwezruizweuiriuwzruizwiuezriuzweuirziuzzzz}";
+
+            SMSCharacterCalculator.SMSValidation actual = SMSCharacterCalculator.GetSMSValidation(text);
+
+            Assert.IsFalse(actual.IsLonger);
+            Assert.IsTrue(actual.TextLength == 308);
+            Assert.IsTrue(actual.SMSCount == 3);
+            Assert.IsTrue(actual.OptimizedText == text);
+        }
+
+        [TestMethod]
+        public void GetSMSValidation_With_Long_SMS()
+        {
+            string text =
+                "{abvgfhtrururufhdfjhdjhfghfggfhfhiiiourzoruozuiorutiozuouuzuiortuziouriozuriozuiourzirtuziuituziouriotuzurzuriotuziutruziorioutruziutiuriturtzotioziortuzioiozutzutruzfhjdjfhhjhjjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhhjhjjhjjjjhjhjhjhhjhhhhuuuuuuuuuuuuuuuuuuuujdfhjkfhjksuizueuruiewrziwuezrizweiurzwuiezruiweuirweuizruiwezruizweuirwzeurzweuiriuwzeuirrwezruizweuiriuwzruizwiuezriuzweuirziuzzzz}";
+
+            SMSCharacterCalculator.SMSValidation actual = SMSCharacterCalculator.GetSMSValidation(text);
+
+            Assert.IsTrue(actual.IsLonger);
+            Assert.IsTrue(actual.TextLength == 461);
+            Assert.IsTrue(actual.SMSCount == 3);
+            Assert.IsTrue(actual.OptimizedText == text);
+        }
+    }
+}
+    
---
+++ SMSCharacterCalculator/SMSCharacterCalculator.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMSCharacterCalculator
+{
+    public static class SMSCharacterCalculator
+    {
+        public class SMSValidation
+        {
+            public bool IsLonger { get; set; }
+
+            public int TextLength { get; set; }
+
+            public int SMSCount { get; set; }
+
+            public string OptimizedText { get; set; }
+        }
+
+        public static SMSValidation GetSMSValidation(string text, bool optimize = true)
+        {
+            SMSValidation smsValidation = new SMSValidation();
+
+            if (optimize)
+            {
+                text = OptimizeText(text);
+            }
+
+            smsValidation.OptimizedText = text;
+
+            smsValidation.TextLength = CalculateLength(text);
+
+            const int firstSMSLengthMax = 160;
+            const int secondSMSLengthMax = 306;
+            const int thirdSMSLengthMax = 459;
+
+            smsValidation.IsLonger = smsValidation.TextLength > thirdSMSLengthMax;
+
+            if (smsValidation.TextLength <= firstSMSLengthMax)
+            {
+                smsValidation.SMSCount = 1;
+            }
+            else
+            {
+                if (smsValidation.TextLength <= secondSMSLengthMax)
+                {
+                    smsValidation.SMSCount = 2;
+                }
+                else
+                {
+                    smsValidation.SMSCount = 3;
+                }
+            }
+
+            return smsValidation;
+        }
+
+        internal static int CalculateLength(string text)
+        {
+            int length = 0;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return length;
+            }
+
+            foreach (char c in text)
+            {
+                if (CharactersWithTwoLength.Contains(c))
+                {
+                    length = length + 2;
+                }
+                else
+                {
+                    length = length + 1;
+                }
+            }
+
+            return length;
+        }
+
+        internal static string OptimizeText(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                sb.Append(OptimizeCharacter(c));
+            }
+
+            return sb.ToString();
+        }
+
+        private static char OptimizeCharacter(char character)
+        {
+            char temp;
+
+            if (!UnicodeToGSM.TryGetValue(character, out temp))
+            {
+                temp = character;
+            }
+
+            return temp;
+        }
+
+        private static readonly Dictionary<char, char> UnicodeToGSM = new Dictionary<char, char>
+        {
+            {'á', 'à'},
+            {'í', 'ì'},
+            {'ó', 'ò'},
+            {'ú', 'ù'},
+            {'ő', 'ö'},
+            {'ű', 'ü'},
+            {'Á', 'Å'},
+            {'Í', 'I'},
+            {'Ú', 'U'},
+            {'Ó', 'O'},
+            {'Ő', 'Ö'},
+            {'Ű', 'Ü'}
+        };
+
+        private static readonly char[] CharactersWithTwoLength = {'|', '^', '€', '{', '}', '[', '~', ']', '\\'};
+    }
+}
